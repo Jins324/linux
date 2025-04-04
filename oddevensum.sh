42a82c2e94d27bdf21ea54b6901748a36e1a215d
Jins324 <jinssabu203@gmail.com>
2025-04-04 14:50:45 +0530

    Add files via upload

---
+++ oddevensum.sh
@@ -0,0 +1,15 @@
+echo "enter a set of numbers seperated by spaces:"
+read -a numbers
+sum_even=0
+sum_odd=0
+for num in "${numbers[@]}"
+do
+  if [ $(($num % 2)) -eq 0 ]
+  then
+     sum_even=$(($sum_even + $num))
+  else
+     sum_odd=$(($sum_odd + $num))
+  fi
+done
+echo "sum of even numbers is: $sum_even"        
+echo "sum of odd numbers is: $sum_odd"
